8c49194bbe07b58f4b2d00f392025d96144d7a88
etr0n <56270962+etr0n@users.noreply.github.com>
2020-12-22 14:26:36 +0200

    Manage controller updated

--- Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -34,20 +34,20 @@ namespace GrozioSalonuISCF.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "Laukelis negali būti tuščias")]
             [DataType(DataType.Password)]
             [Display(Name = "Current password")]
             public string OldPassword { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Laukelis negali būti tuščias")]
+            [StringLength(100, ErrorMessage = "Laukelis Naujas slaptažodis turi būti bent {2} ir daugiausiai {1} simbolių ilgio.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Compare("NewPassword", ErrorMessage = "Naujas slaptažodis ir Patvirtinti naują slaptažodį nesutampa!")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -92,8 +92,8 @@ namespace GrozioSalonuISCF.Areas.Identity.Pages.Account.Manage
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Your password has been changed.";
+            _logger.LogInformation("Naudoto slaptažodis pakeistas sėkmnigai!");
+            StatusMessage = "Jūsų slaptažodis pakeistas.";
 
             return RedirectToPage();
         }
